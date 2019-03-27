7543a5bb2c4d744d575b4ecf17e2a93514f6cadc
rahimi0151 <rahimi0151@gmail.com>
2019-03-27 23:06:42 +0430

    bugfix

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -16,19 +16,7 @@
                             <a href="{{ route('user.show', ['user' => $user]) }}"> {{ $user->username }}
                         </a></h2></b>
 
-                        {{-- user can delete his/her posts --}}
-                        @if ($user->id == Auth::id())
-                            <a href="{{ route('posts.edit', ['post' => $post]) }}">
-                                <button class="btn btn-primary">Edit</button>
-                            </a>
-                        @endif
-
                         <hr>
-                        <p>Posted: {{ $post->created_at }}</p>
-                    @endslot
-                    <p>{{ $post->body }}</p>
-                    <hr>
-                    <p>{{ $post->claps }} claps!</p>
                 @endcomponent
                 <br>
             @endforeach
